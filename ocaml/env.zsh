47380b90cfd6d12d000062f6e5678e80a3a54acd
Zsolt Dollenstein <zsol.zsol@gmail.com>
2016-08-04 13:13:28 +0100

    add fb stuff

---
+++ ocaml/env.zsh
@@ -0,0 +1,4 @@
+
+
+# OPAM configuration
+. /Users/zsol/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
--- osx/env.zsh
+++ osx/env.zsh
@@ -1,1 +1,2 @@
 export PKG_CONFIG_PATH="/usr/X11/lib/pkgconfig:$PKG_CONFIG_PATH"
+export ANDROID_HOME=${ANDROID_HOME:-/usr/local/opt/android-sdk}
